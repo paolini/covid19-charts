11085728e90911ae1f1a818dfc5eb567c3bc7783
Emanuele Paolini <emanuele.paolini@gmail.com>
2020-03-24 12:08:10 +0100

    update hopkins data sources

--- dataset.js
+++ dataset.js
@@ -294,7 +294,8 @@ class HopkinsConfirmedDataset extends HopkinsDataset {
     constructor() {
         super({
             name: "confirmed",
-            path: "csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+            // path: "csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+            path: "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
             fields: ['confirmed'],
         });
     }
@@ -304,7 +305,8 @@ class HopkinsDeathsDataset extends HopkinsDataset {
     constructor() {
         super({
             name: "deaths",
-            path: "csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+            // path: "csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+            path: "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
             fields: ['deaths'],
         });
     }
